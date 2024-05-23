a9bbc82af003a8e200af3dc486048a8fbe9cae36
gigas64 <gigas64@opengrass.net>
2024-05-23 13:39:13 -0500

    - add Controller

--- nn_rag/__init__.py
+++ nn_rag/__init__.py
@@ -1,4 +1,5 @@
 from nn_rag.components.knowledge import Knowledge
+from nn_rag.components.controller import Controller
 
 # release version number picked up in the setup.py
-__version__ = '0.0.15'
+__version__ = '0.0.16'
